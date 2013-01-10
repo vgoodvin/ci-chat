b04cc3e619d01be2ca11c93d93379a802ae03d4f
Vasily Gudoshnikov <vgoodvin@gmail.com>
2013-01-10 20:38:05 +0400

    Added database migration controller file.

---
+++ application/controllers/migrations.php
@@ -0,0 +1,13 @@
+<?php
+
+class Migrations extends CI_Controller {
+
+  public function index()
+  {
+    $this->load->library('migration');
+
+    if ( ! $this->migration->current()) {
+      show_error($this->migration->error_string());
+    }
+  }
+}
